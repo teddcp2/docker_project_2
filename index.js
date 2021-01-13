d4991e79c47632d4ce0c5b19de7b9a180cbf9b7f
tedd <teddfords.ml@gmail.com>
2021-01-13 13:01:31 +0530

    docker project 2

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const redis = require("redis");
+
+const client = redis.createClient({
+  host: "redis",
+  port: 6379,
+});
+
+const app = express();
+
+// echo redis errors to the console
+client.on("error", (err) => {
+  console.log("Error " + err);
+});
+
+// setting the number of visitors
+client.set("visitorsCount", 0);
+
+app.get("/", (req, res) => {
+  const keyRequired = "vistorsCount";
+
+  return client.get(keyRequired, (err, data) => {
+    if (res) {
+      client.set(keyRequired, data + 1);
+      return res.json({ count: data });
+    }
+  });
+});
+
+app.listen(80);
+
+//https://medium.com/tech-tajawal/introduction-to-caching-redis-node-js-e477eb969eab
